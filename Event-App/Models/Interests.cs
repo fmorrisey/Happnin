6dce3ea35c0d50114997623c2db9d9034728be81
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-12-03 15:05:27 -0600

    Interest table

---
+++ Event-App/Models/Interests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Models
+{
+    public class Interests
+    {
+        [Key]
+        public int InterestId { get; set; }
+        public string Music { get; set; }
+        public string Food { get; set; }
+        public string CommunityEvent { get; set; }
+        public string Festival { get; set; }
+        public string Party { get; set; }
+        public string Football { get; set; }
+        public string BasketBall { get; set; }
+        public string BaseBall { get; set; }
+        public string Running { get; set; }
+        public string Biking  { get; set; }
+        public string Hiking { get; set; }
+        public string Play { get; set; }
+        
+
+    }
+}
