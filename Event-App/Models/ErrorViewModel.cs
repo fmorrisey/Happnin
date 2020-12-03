68fd4c764bbc9a8261f0ef02ff9bc9e7bac52104
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-12-03 08:46:53 -0600

    Intial Project Upload w/ Newtonsoft JSON

---
+++ Event-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Event_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
