b378e1f1fcd30e15f3b8525429b802f61a7f5436
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-03 15:36:47 -0600

    data seed for days

--- Event-App/Data/ApplicationDbContext.cs
+++ Event-App/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Event_App.Models.Calendar;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -39,6 +40,11 @@ namespace Event_App.Data
               .HasData(
               new Day
               {
+                  Id = 0,
+                  WeekDay = "Sunday"
+              },
+              new Day
+              {
                   Id = 1,
                   WeekDay = "Monday"
               }, new Day
@@ -57,6 +63,11 @@ namespace Event_App.Data
               {
                   Id = 5,
                   WeekDay = "Friday"
+              },
+              new Day
+              {
+                  Id = 6,
+                  WeekDay = "Saturday"
               });
 
         }
