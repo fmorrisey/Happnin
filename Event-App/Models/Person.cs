7a24ddbfdfbf7fa254d8315215e408839c628506
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-12-03 14:43:49 -0600

    Person Model

---
+++ Event-App/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Models
+{
+    public class Person
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser identityUser { get; set; }
+
+    }
+}
