35e4b602b8109cef4e3c2375abf63067fe9d775e
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-12-03 14:52:52 -0600

    Linking table for User Interests

---
+++ Event-App/Models/UserInterests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Models
+{
+    public class UserInterests
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser identityUser { get; set; }
+        public int InterestId { get; set; }
+    }
+}
