915cf27132a18deb01e12e863e0a98b52ba94c50
Christopher Couch <c.alan.couch@gmail.com>
2020-12-04 14:17:23 -0600

    Removed the seeded data for the days of the week, added migration and updated database

--- Event-App/Data/ApplicationDbContext.cs
+++ Event-App/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Event_App.Models.Calendar;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,42 +12,6 @@ namespace Event_App.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Day>()
-              .HasData(
-              new Day
-              {
-                  Id = 0,
-                  WeekDay = "Sunday"
-              },
-              new Day
-              {
-                  Id = 1,
-                  WeekDay = "Monday"
-              }, new Day
-              {
-                  Id = 2,
-                  WeekDay = "Tuesday"
-              }, new Day
-              {
-                  Id = 3,
-                  WeekDay = "Wednesday"
-              }, new Day
-              {
-                  Id = 4,
-                  WeekDay = "Thursday"
-              }, new Day
-              {
-                  Id = 5,
-                  WeekDay = "Friday"
-              },
-              new Day
-              {
-                  Id = 6,
-                  WeekDay = "Saturday"
-              });
 
         }
 
