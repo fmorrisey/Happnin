21adaf66341a5e5985845cb59c0b70ad0352bd2f
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-12-04 11:36:53 -0600

    Interest Table creation

---
+++ Event-App/Models/Interests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Models
+{
+    public class Interests
+    {
+        [Key]
+        int InterestId { get; set; }
+        [Display(Name = "Interest Description")]
+        string InterestType { get; set; }
+
+    }
+}
