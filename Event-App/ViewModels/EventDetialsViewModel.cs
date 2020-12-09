c8495c7ee5c987361fc10342889801e8427ec918
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-09 11:21:42 -0600

    finished the linq queries for controller, ON THE FIRST TRY!

---
+++ Event-App/ViewModels/EventDetialsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Models
+{
+    public class EventDetialsViewModel
+    {
+        public Event deatilEvent { get; set; }
+        public Person host { get; set; }
+        public Interest interest { get; set; }
+        public Address address { get; set; }
+
+    }
+}
