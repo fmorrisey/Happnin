acc3da1db3731a281a49ecc25f37dcb0f12d11ed
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-12-09 11:27:39 -0600

    updates to events and ticket api - not complete

---
+++ Event-App/Services/PublicEvents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_App.Services
+{
+    public class PublicEvents
+    {
+    }
+}
